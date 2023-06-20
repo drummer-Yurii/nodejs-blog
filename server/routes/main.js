157c8cbb594d68c1b340ea328d8a7334b3703fdb
Yurii Rybachok <drummer2009@ukr.net>
2023-06-20 15:50:40 +0300

    pagination for posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,14 +7,30 @@ GET
 HOME
 */
 router.get('', async (req, res) => {
-    const locals = {
-        title: "NodeJS Blog",
-        description: "Simple Blog created with NodeJS, Express & MongoDB"
-    }
-
     try {
-        const data = await Post.find();
-        res.render('index', { locals, data });
+        const locals = {
+            title: "NodeJS Blog",
+            description: "Simple Blog created with NodeJS, Express & MongoDB"
+        }
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: { createdAt: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', { 
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null 
+        });
+
     } catch (error) {
         console.log(error);
     }
