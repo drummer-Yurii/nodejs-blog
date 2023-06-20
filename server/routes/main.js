4d3eb2c29a4101e48b40d79bcc2e2274330238c1
Yurii Rybachok <drummer2009@ukr.net>
2023-06-20 16:49:25 +0300

    create search view & logic for search posts

--- server/routes/main.js
+++ server/routes/main.js
@@ -56,7 +56,36 @@ router.get('/post/:id', async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-})
+});
+
+/* 
+POST /
+Post searchTerm
+*/
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Simple Blog created with NodeJS, Express & MongoDB"
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+        
+        const data = await Post.find({
+            $or: [
+                { title: { $regex: new RegExp(searchNoSpecialChar, 'i') }},
+                { body: { $regex: new RegExp(searchNoSpecialChar, 'i') }}
+            ]
+        });
+        res.render("search", {
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 
 
