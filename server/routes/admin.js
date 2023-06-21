d21ae22c9ad147dbca7f4d6387fb426a1191d876
Yurii Rybachok <drummer2009@ukr.net>
2023-06-21 12:00:43 +0300

    create admin panel page with login & register

---
+++ server/routes/admin.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+const adminLayout = '../views/layouts/admin';
+
+/* 
+GET
+Admin - Login Page
+*/
+router.get('/admin', async (req, res) => {
+    try {
+        const locals = {
+            title: "Admin",
+            description: "Simple Blog created with NodeJS, Express & MongoDB"
+        }
+
+        res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports = router;
