ef296e32da30cc59124b4e3c50dc9c064c348d10
Yurii Rybachok <drummer2009@ukr.net>
2023-06-21 15:16:40 +0300

    create new post & save to db

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -119,6 +119,29 @@ router.get('/add-post', authMiddleware, async (req, res) => {
 });
 
 
+/* 
+POST
+Admin - Create New Post
+*/
+router.post('/add-post', authMiddleware, async (req, res) => {
+    try {
+        try {
+            const newPost = new Post({
+                title: req.body.title,
+                body: req.body.body
+            });
+
+            await Post.create(newPost);
+            res.redirect('/dashboard');
+        } catch (error) {
+            console.log(error);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
 // router.post('/admin', async (req, res) => {
 //     try {
 //         const { username, password } = req.body;
